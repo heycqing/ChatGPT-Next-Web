0bc69c94ffc32220ad3e5da01719661ad05e057f
wusiqing <siqingiii@163.com>
2023-05-31 18:54:59 +0800

    update: 开发

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT by halohot</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          建立自己的私人助理
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
